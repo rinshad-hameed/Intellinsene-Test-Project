89f9c8be9a4df223fd4a52a0304c209852a6b5b5
rinshadhameed <rinshadh@gmail.com>
2022-03-24 15:53:38 +0000

    code review changes

--- src/test/java/tests/SmokeTests.java
+++ src/test/java/tests/SmokeTests.java
@@ -7,28 +7,22 @@ import org.testng.annotations.Test;
 import pages.*;
 import utils.ConfigFileReader;
 
-;
 
 public class SmokeTests extends TestBase {
 
 
     @BeforeTest
     @Parameters({"username","password"})
-    public void Login(String userName, String password) throws InterruptedException {
+    public void Login(String userName, String password) {
         String decodedPass = ConfigFileReader.decryptText(password);
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(userName,decodedPass);
-
-    //    Assert.assertTrue(homePage.isCartPageLoaded());
     }
 
     @Test(priority=2)
     public void verifySearchResults() throws InterruptedException {
-        HomePage homePage = new HomePage(driver);
         SearchPage searchPage = new SearchPage(driver);
-        //homePage.navigateToDashBoard();
         searchPage.verifySearchResults("stockpile");
-        //    Assert.assertTrue(homePage.isCartPageLoaded());
     }
     @Test(priority=1)
     public void verifyAddSingularData() throws InterruptedException {
@@ -36,8 +30,8 @@ public class SmokeTests extends TestBase {
         HomePage homePage = new HomePage(driver);
         homePage.navigateToDashBoard();
         dashPage.verifyAddSingularData("78");
-        //    Assert.assertTrue(homePage.isCartPageLoaded());
     }
+
     //Negative tests
     @Test(priority=4)
     public void verifyInvalidLogin() throws InterruptedException {
@@ -51,9 +45,7 @@ public class SmokeTests extends TestBase {
 
     @Test(priority=3)
     public void verifyDuplicateDashboard() throws InterruptedException {
-        DashBoardPage dashPage = new DashBoardPage(driver);
         HomePage homePage = new HomePage(driver);
-    //    homePage.navigateToDashBoard();
         homePage.verifyDuplicateDashboard();
 
     }
